8f872dd3d28c1f3c36db4f8a6928c4a86a3c2c60
Ashish Raj Singh <75162873+ashish757@users.noreply.github.com>
2022-06-21 18:31:19 +0530

    click event

--- script.js
+++ script.js
@@ -14,6 +14,21 @@
     navLinks.classList.remove("mobActive");
   
   }
+
+  const hasSubMenus = document.querySelectorAll(".has-submenu")
+
+  Array.from(hasSubMenus).forEach(li => {
+    li.addEventListener('click', (e) => {
+      e.stopPropagation();
+      console.log(e.target.closest('li'));
+      e.target.closest('li').classList.toggle("submenu-active")
+    })
+
+    li.addEventListener('mouseleave', (e) => {
+      e.target.closest('li').classList.remove("submenu-active")
+    })
+  })
+
   
   
   // sidebar submenu open close js code
