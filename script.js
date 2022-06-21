939a92219f781cb5056a0f57b2a592d72b70291a
Ashish Raj Singh <75162873+ashish757@users.noreply.github.com>
2022-06-21 19:21:50 +0530

    footer

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-  // search-box open close js code
-  let navbar = document.querySelector(".navbar");
  
-  
-  // sidebar open close js code
   let navLinks = document.querySelector(".nav-links");
   let menuOpenBtn = document.querySelector(".navbar .navbar-open");
   let menuCloseBtn = document.querySelector(".nav-links .navbar-close");
@@ -30,18 +26,4 @@
   })
 
   
-  
-  // sidebar submenu open close js code
-  // let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-  // htmlcssArrow.onclick = function() {
-  //  navLinks.classList.toggle("show1");
-  // }
-  // let moreArrow = document.querySelector(".more-arrow");
-  // moreArrow.onclick = function() {
-  //  navLinks.classList.toggle("show2");
-  // }
-  // let jsArrow = document.querySelector(".js-arrow");
-  // jsArrow.onclick = function() {
-  //  navLinks.classList.toggle("show3");
-  // }
   
