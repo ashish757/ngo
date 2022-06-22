56caa434adb23057232127e995f3096b1371b243
Ashish Raj Singh <75162873+ashish757@users.noreply.github.com>
2022-06-22 18:44:46 +0530

    more

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
- 
-  let navLinks = document.querySelector(".nav-links");
-  let menuOpenBtn = document.querySelector(".navbar .navbar-open");
-  let menuCloseBtn = document.querySelector(".nav-links .navbar-close");
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .navbar-open");
+let menuCloseBtn = document.querySelector(".nav-links .navbar-close");
 
-  menuOpenBtn.onclick = function() {
+menuOpenBtn.onclick = function () {
   navLinks.classList.add("mobActive");
-  }
-  menuCloseBtn.onclick = function() {
-    navLinks.classList.remove("mobActive");
-  
-  }
+};
+menuCloseBtn.onclick = function () {
+  navLinks.classList.remove("mobActive");
+};
 
-  const hasSubMenus = document.querySelectorAll(".has-submenu")
+const hasSubMenus = document.querySelectorAll(".has-submenu");
 
-  Array.from(hasSubMenus).forEach(li => {
-    li.addEventListener('click', (e) => {
-      e.stopPropagation();
-      console.log(e.target.closest('li'));
-      e.target.closest('li').classList.toggle("submenu-active")
-    })
+Array.from(hasSubMenus).forEach((li) => {
+  li.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log(e.target.closest("li"));
+    e.target.closest("li").classList.toggle("submenu-active");
+  });
 
-    li.addEventListener('mouseleave', (e) => {
-      e.target.closest('li').classList.remove("submenu-active")
-    })
-  })
+  li.addEventListener("mouseleave", (e) => {
+    e.target.closest("li").classList.remove("submenu-active");
+  });
+});
 
-  
-  
+const parallax = document.querySelector(".parallax");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY =
+    (offset - parallax.offsetTop) * 0.5 + "px";
+  // console.log("Parallax : " + parallax.offsetTop + " / Window: " + offset);
+  // console.log("real : " + (offset - parallax.offsetTop) + " Reel: " + (offset - parallax.offsetTop) * 0.5);
+});
